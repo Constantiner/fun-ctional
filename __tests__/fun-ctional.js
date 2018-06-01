5f8f21d7f2d5a1a4a74c15719ed0f1cc289a5544
Konstantin Kovalev <constantiner@gmail.com>
2018-06-01 15:43:01 +0200

    Minor syntax corrections for more succinct code

--- __tests__/fun-ctional.js
+++ __tests__/fun-ctional.js
@@ -3,9 +3,9 @@ import { acompose, apipe } from "../src/fun-ctional";
 const getErrorMessage = value => `Error with value "${value}"`;
 const getError = value => new Error(getErrorMessage(value));
 const createAsyncPromise = (valueToResolve, successful = true) =>
-	new Promise((resolve, reject) => {
-		setTimeout(() => (successful ? resolve(valueToResolve) : reject(getError(valueToResolve))), 100);
-	});
+	new Promise((resolve, reject) =>
+		setTimeout(() => (successful ? resolve(valueToResolve) : reject(getError(valueToResolve))), 100)
+	);
 const createSyncPromise = (valueToResolve, successful = true) =>
 	successful ? Promise.resolve(valueToResolve) : Promise.reject(getError(valueToResolve));
 
