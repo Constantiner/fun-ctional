e21115ae9eec09852fd3a9231c10438027baba4e
Konstantin Kovalev <constantiner@gmail.com>
2018-06-06 17:56:30 +0200

    Added building minified files Fixed issues of generating source maps for unminified ES5 files.

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,9 @@ const babel = require("gulp-babel");
 const del = require("del");
 const sourcemaps = require("gulp-sourcemaps");
 const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const file = require("gulp-file");
+const flatmap = require("gulp-flatmap");
 const MAIN_FILE_NAME = "fun-ctional";
 const BROWSERS = [">0.25%", "not ie 11", "not op_mini all"];
 const BABEL_PLUGINS = ["transform-es2015-modules-umd"];
@@ -40,10 +43,36 @@ gulp.task("es6modules", () =>
 		.pipe(getDest())
 );
 
-gulp.task("mainEs5umd", () =>
-	getSourceFile()
+gulp.task("proceedMainEs5umd", ["emptyBannerFile"], () =>
+	gulp
+		.src(["tmp/banner.js", "src/**/*.js"])
+		.pipe(sourcemaps.init())
+		.pipe(
+			babel({
+				presets: [
+					[
+						"env",
+						{
+							targets: {
+								browsers: BROWSERS
+							}
+						}
+					]
+				],
+				plugins: BABEL_PLUGINS
+			})
+		)
 		.pipe(concat(`${MAIN_FILE_NAME}.js`))
+		.pipe(sourcemaps.write("."))
+		.pipe(getDest())
+);
+
+gulp.task("mainEs5umd", ["proceedMainEs5umd"], () => del(["tmp"]));
+
+gulp.task("mainEs5umdMinified", () =>
+	getSourceFile()
 		.pipe(sourcemaps.init())
+		.pipe(concat(`${MAIN_FILE_NAME}.min.js`))
 		.pipe(
 			babel({
 				presets: [
@@ -59,12 +88,41 @@ gulp.task("mainEs5umd", () =>
 				plugins: BABEL_PLUGINS
 			})
 		)
-		.pipe(getBanner())
+		.pipe(uglify())
 		.pipe(sourcemaps.write("."))
 		.pipe(getDest())
 );
 
-gulp.task("es5umds", () =>
+gulp.task("proceedEs5umds", ["emptyBannerFile"], () =>
+	getSourceFile()
+		.pipe(
+			flatmap((stream, file) =>
+				gulp
+					.src(["tmp/banner.js", file.path])
+					.pipe(sourcemaps.init())
+					.pipe(
+						babel({
+							presets: [
+								[
+									"env",
+									{
+										targets: {
+											browsers: BROWSERS
+										}
+									}
+								]
+							],
+							plugins: BABEL_PLUGINS
+						})
+					)
+					.pipe(concat(file.relative))
+					.pipe(sourcemaps.write("."))
+			)
+		)
+		.pipe(getDest())
+);
+
+gulp.task("es5umdsMinified", () =>
 	getSourceFile()
 		.pipe(sourcemaps.init())
 		.pipe(
@@ -82,11 +140,22 @@ gulp.task("es5umds", () =>
 				plugins: BABEL_PLUGINS
 			})
 		)
-		.pipe(getBanner())
+		.pipe(uglify())
+		.pipe(rename({ extname: ".min.js" }))
 		.pipe(sourcemaps.write("."))
 		.pipe(getDest())
 );
 
-gulp.task("scripts", () => gulp.start("mainEs6module", "mainEs5umd", "es6modules", "es5umds"));
+gulp.task("es5umds", ["proceedEs5umds"], () => del(["tmp"]));
+
+gulp.task("emptyBannerFile", () =>
+	file("./tmp/banner.js", "", { src: true })
+		.pipe(getBanner())
+		.pipe(gulp.dest("."))
+);
+
+gulp.task("scripts", () =>
+	gulp.start("mainEs6module", "mainEs5umd", "mainEs5umdMinified", "es6modules", "es5umds", "es5umdsMinified")
+);
 
 gulp.task("default", ["clean"], () => gulp.start("scripts"));
