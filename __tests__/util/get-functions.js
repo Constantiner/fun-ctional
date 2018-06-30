bd55f96a3740c8842ceae9f58dbcf55eed9514b2
Konstantin Kovalev <constantiner@gmail.com>
2018-06-30 17:48:29 +0200

    Add tests for getFunctions from arguments utility

--- __tests__/util/get-functions.js
+++ __tests__/util/get-functions.js
@@ -11,4 +11,26 @@ describe("getFunctions tests", () => {
 		const result = getFunctions(input);
 		expect(result).toBe(input);
 	});
+	it("should return the same empty array", () => {
+		const input = [];
+		const result = getFunctions(input);
+		expect(result).toBe(input);
+	});
+	it("should return empty array for call without arguments", () => {
+		const result = getFunctions();
+		expect(result).toEqual([]);
+	});
+	it('should return array from first parameter as array', () => {
+		const firstParam = [n => n, n => n + 5];
+		const input = [firstParam];
+		const result = getFunctions(input);
+		expect(result).toEqual(firstParam);
+	});
+	it('should return array from first parameter as iterable', () => {
+		const firstParamInitValue = [n => n, n => n + 5];
+		const firstParam = new Set(firstParamInitValue);
+		const input = [firstParam];
+		const result = getFunctions(input);
+		expect(result).toEqual(firstParamInitValue);
+	});
 });
--- src/util/get-functions.js
+++ src/util/get-functions.js
@@ -1,9 +1,9 @@
 import isIterable from "./is-iterable";
 /**
  * Returns an array of functions.
- * 
+ *
  * @param {array} fns Are input array (just arguments of some other function).
  * If if consists of one element and this element is an iterable returns array from it.
  * @returns {array} Resulting array of functions.
  */
-export default fns => (fns.length === 1 && isIterable(fns[0]) ? Array.from(fns[0]) : fns);
+export default fns => (fns ? (fns.length === 1 && isIterable(fns[0]) ? Array.from(fns[0]) : fns) : []);
