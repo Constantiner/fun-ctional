66a79145118604cc1a34406fd5b18ffb71646771
Konstantin Kovalev <constantiner@gmail.com>
2018-07-09 12:04:50 +0200

    Added imports of recently added functions to the main library file

--- src/fun-ctional.js
+++ src/fun-ctional.js
@@ -1,7 +1,11 @@
+import acatch from "./acatch";
 import acompose from "./acompose";
 import allFromList from "./allFromList";
 import allTheSame from "./allTheSame";
 import amap from "./amap";
 import apipe from "./apipe";
+import areduce from "./areduce";
+import areduceRight from "./areduceRight";
+import withCatch from "./withCatch";
 
-export { acompose, allFromList, allTheSame, amap, apipe };
+export { acatch, acompose, allFromList, allTheSame, amap, apipe, areduce, areduceRight, withCatch };
