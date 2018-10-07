df1f876bfad0d44f676dad2ca3e9426990a78e05
Konstantin Kovalev <constantiner@gmail.com>
2018-10-07 14:33:35 +0200

    Remove allFromList function (previously an amap alias)

--- src/fun-ctional.js
+++ src/fun-ctional.js
@@ -1,6 +1,5 @@
 import acatch from "./acatch";
 import acompose from "./acompose";
-import allFromList from "./allFromList";
 import allTheSame from "./allTheSame";
 import amap from "./amap";
 import apipe from "./apipe";
@@ -8,4 +7,4 @@ import areduce from "./areduce";
 import areduceRight from "./areduceRight";
 import withCatch from "./withCatch";
 
-export { acatch, acompose, allFromList, allTheSame, amap, apipe, areduce, areduceRight, withCatch };
+export { acatch, acompose, allTheSame, amap, apipe, areduce, areduceRight, withCatch };
