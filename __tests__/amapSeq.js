44c02bd50e36fe4f055b9c44ef32fce401cce85a
Konstantin Kovalev <constantiner@gmail.com>
2018-10-08 14:06:50 +0200

    Add amapSeq function
    Perform formatting

--- __tests__/amapSeq.js
+++ __tests__/amapSeq.js
@@ -1,14 +1,14 @@
-import amap from "../src/amap";
+import amapSeq from "../src/amapSeq";
 import { mockFnExpectations, squareMock } from "./test-utils/jestMockFns";
 
-describe("amap tests", () => {
+describe("amapSeq tests", () => {
 	it("should work for base case", async () => {
 		expect.assertions(6);
 		const input1 = 4;
 		const input2 = 5;
 		const inputValue = [input1, input2];
 		const square = squareMock(jest, "square");
-		const result = await amap(square)(inputValue);
+		const result = await amapSeq(square)(inputValue);
 		expect(result).toEqual([16, 25]);
 		expect(square).toHaveBeenCalledTimes(2);
 		mockFnExpectations(square, 1, 16, input1, 0, inputValue);
--- src/acatch.js
+++ src/acatch.js
@@ -1,22 +1,22 @@
 /**
  * Composable version of catch method for promises.
- * 
+ *
  * It gets a value (a promise or not), resolves it and if resulting promise was rejected calls catch function.
- * 
+ *
  * It allows to handle errors within acompose or apipe asynchronous composition chains to restore broken state etc.
- * 
+ *
  * A sample with acompose:
  *
  * <pre><code>const resultOrFallback = await acompose(acatch(handleAndRecoverFn), canFailFn)(someInput);</code></pre>
- * 
+ *
  * Standalone usage:
  *
  * <pre><code>const resultOrFallback = await acatch(handleAndRecoverFn)(requestDataAndReturnPromise());</code></pre>
- * 
+ *
  * It is the same as the following:
- * 
+ *
  * <pre><code>requestDataAndReturnPromise().catch(handleAndRecoverFn).then(resultOrFallback => console.log(resultOrFallback));</code></pre>
- * 
+ *
  * @param {function} catchFn Is function to handle Promise's rejection.
  * @returns {any => Promise} A function which expects any value as input (Promise or not) and returns a Promise.
  */
