65ab473ae1bcdd2ac2a55cc79e54fc757389732c
Konstantin Kovalev <constantiner@gmail.com>
2019-05-21 13:49:58 +0200

    Move browser list from gulpfile into separate configuration

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-const BROWSERS = [">0.25%", "not ie 11", "not op_mini all"];
+const browsers = require("./package.json").browserslist;
 
 module.exports = api => {
 	if (api.env("test")) {
@@ -22,7 +22,7 @@ module.exports = api => {
 				"@babel/preset-env",
 				{
 					targets: {
-						browsers: BROWSERS
+						browsers
 					},
 					modules: false
 				}
