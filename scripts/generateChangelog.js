b119469ad2069e8f93c90087daa46f4f49e778cd
Konstantin Kovalev <constantiner@gmail.com>
2019-05-23 18:36:35 +0200

    Add base changelog generation script

---
+++ scripts/generateChangelog.js
@@ -0,0 +1,13 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const { version } = require("../package.json");
+
+const generateChangelog = async () => {
+	const { stdout, stderr } = await exec(`github_changelog_generator --future-release ${version}`);
+	// eslint-disable-next-line no-console
+	console.log(stdout);
+	// eslint-disable-next-line no-console
+	console.error(stderr);
+};
+
+generateChangelog();
