ed7bf6293a08f2cbccae89434a563dd1a5fee981
Konstantin Kovalev <constantiner@gmail.com>
2019-05-23 20:24:31 +0200

    Generate proper release version in changelog

--- scripts/generateChangelog.js
+++ scripts/generateChangelog.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require("child_process").exec);
 const { version } = require("../package.json");
 
 const generateChangelog = async () => {
-	const { stdout, stderr } = await exec(`github_changelog_generator --future-release ${version}`);
+	const { stdout, stderr } = await exec(`github_changelog_generator --future-release v${version}`);
 	// eslint-disable-next-line no-console
 	console.log(stdout);
 	// eslint-disable-next-line no-console
