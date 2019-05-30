e86d43f11f377ae8c6b6c58b0c0efe8ee4b2b270
Konstantin Kovalev <constantiner@gmail.com>
2019-05-30 17:59:39 +0200

    Optimise distributive for ES6 and CJS modules

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,7 @@
 import del from "del";
 
 const clean = async () =>
-	del(["browser", "*.js", "*.mjs", "*.map", "!babel.config.js", "!jest.config.js", "!rollup.config.js"]);
+	del(["browser", "esm", "*.js", "*.map", "!babel.config.js", "!jest.config.js", "!rollup.config.js"]);
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
