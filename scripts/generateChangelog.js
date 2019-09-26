a4a9781421d4a255bb92ffe8d89c7a8ab1fd6d5a
Konstantin Kovalev <constantiner@gmail.com>
2019-09-26 18:37:02 +0200

    Generate changelogs with ferrarimarco/github-changelog-generator Docker image

--- scripts/generateChangelog.js
+++ scripts/generateChangelog.js
@@ -1,13 +1,24 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+import { exec as childProcessExec } from "child_process";
+import { promisify } from "util";
+
+const exec = promisify(childProcessExec);
 const { version } = require("../package.json");
 
 const generateChangelog = async () => {
-	const { stdout, stderr } = await exec(`github_changelog_generator --future-release v${version}`);
+	// const { stdout, stderr } = await exec(`github_changelog_generator --future-release v${version}`);
+	const command = `docker run --rm -v "$(pwd)":/usr/local/src/your-app -e CHANGELOG_GITHUB_TOKEN='${process.env.CHANGELOG_GITHUB_TOKEN}' ferrarimarco/github-changelog-generator --user Constantiner --project fun-ctional --future-release v${version} --header="---\nid: CHANGELOG\ntitle: Changelog\nsidebar_label: Changelog\n---"`;
+	const { stdout, stderr } = await exec(command);
 	// eslint-disable-next-line no-console
 	console.log(stdout);
 	// eslint-disable-next-line no-console
 	console.error(stderr);
 };
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on("unhandledRejection", error => {
+	throw error;
+});
 
 generateChangelog();
